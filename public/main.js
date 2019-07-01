f9ee8b5c569e358adc245a733c82be5baf9ce98e
SashaOnoshko <onoshkosasha99@gmail.com>
2019-07-01 20:13:33 +0300

    initial commit

---
+++ public/main.js
@@ -0,0 +1,77 @@
+
+let tr_ship_1_check = false;
+let tr_ship_2_check = false;
+let tr_ship_1_arr = [];
+let tr_ship_2_arr = [];
+
+function fun(el) {
+    if (tr_ship_1_check === true){
+        
+        tr_ship_1_arr.push(el);
+        if (tr_ship_1_arr.length>2){
+            document.getElementById('status').innerText = 'ship was set';
+            tr_ship_1_check = false;
+            document.getElementById('tr_ship_1').disabled = true;
+        }
+        if (tr_ship_1_arr.length===3) {
+            set_ship1();
+        }
+    }
+
+    if (tr_ship_2_check === true){
+        tr_ship_2_arr.push(el);
+        if (tr_ship_2_arr.length>2){
+            document.getElementById('status').innerText = 'ship was set';
+            tr_ship_2_check = false;
+            document.getElementById('tr_ship_2').disabled = true;
+        }
+        if (tr_ship_2_arr.length===3){
+            set_ship2();
+        }
+    }
+}
+
+function tr_ship_1() {
+    tr_ship_1_check = true;
+    document.getElementById('status').innerText = 'chose 3 cell';
+}
+function tr_ship_2() {
+    tr_ship_2_check = true;
+    document.getElementById('status').innerText = 'chose 3 cell';
+}
+
+function set_ship1() {
+    tr_ship_1_arr.forEach((el)=>{
+        document.getElementById(el).style.backgroundColor='black';
+    });
+    checkArraysBeforeStart();
+}
+function set_ship2() {
+    tr_ship_2_arr.forEach((el)=>{
+        document.getElementById(el).style.backgroundColor='black';
+    });
+    checkArraysBeforeStart();
+}
+
+function checkArraysBeforeStart() {
+    if (tr_ship_1_arr.length===3 && tr_ship_2_arr.length===3){
+        document.getElementById('start_game').disabled = false;
+    }
+}
+function startGame() {
+    document.getElementById('field2').style.display = 'block';
+}
+
+
+function battle(el) {
+    if(tr_ship_1_arr.indexOf(el.slice(0,7)+'_1') >= 0){
+        document.getElementById(el).style.backgroundColor='red';
+    }
+    else if(tr_ship_2_arr.indexOf(el.slice(0,7)+'_1') >= 0){
+        document.getElementById(el).style.backgroundColor='red';
+    }
+    else{
+        document.getElementById(el).style.backgroundColor='gray';
+    }
+
+}
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const server = require("http").createServer(app);
+
+const io = require("socket.io").listen(server);
+
+let users = [];
+connections = [];
+server.listen(process.env.PORT || 3000);
+console.log("server running");
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/");
+});
+
+io.sockets.on("connection", socket => {
+    connections.push(socket);
+    console.log("connected: %s sockets connected", connections.length);
+
+    //Disconnect
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log('disconnected: %s socket connected', connections.length);
+    });
+    //Send ships
+    socket.on('send ships', (ship1, ship2)=>{
+        io.sockets.emit('new ships', {ship1: ship1}, {ship2: ship2});
+    })
+
+});
